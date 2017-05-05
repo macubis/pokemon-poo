84cb40df5967aefc485807cf5c0fa06764be9039
macubis <macubis@gmail.com>
2017-05-04 23:04:09 -0300

    Pokemon terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,26 @@ function Pokemon(nombre, color, poderDeAtaque){
 		pokemon.vida = pokemon.vida - this.poderDeAtaque
 	}
 };
-const Pikachu = new Pokemon("Pikachu","amarillo",100)
-const Charmander = new Pokemon ("Charmander","rojo", 20)
-Pikachu.atacar(Charmander)
-console.log(Charmander.vida);
+	const Pikachu = new Pokemon("Pikachu","amarillo",100);
+	const Charmander = new Pokemon ("Charmander","rojo", 20);
+	const Bulbasaur = new Pokemon("Bulbasaur","verde", 50);
+	const Squirtle = new Pokemon("Squirtle", "cafe", 80);
+
+
+function pelear(nombre,color,poderDeAtaque){ 
+	var res = document.getElementById("resultadoPelea");
+   	var grupoUno = document.getElementById("mySelect").value;
+	var grupoDos = document.getElementById("mySelect2").value;
+	
+		if(grupoUno == grupoDos){		
+		alert("No puedes hacer Pelear dos pokemones Iguales");
+		return;
+		}else{
+			var poder= prompt("Ingrese poder de Ataque de su Pokemon");
+			var primer = new Pokemon(grupoUno, + color, + poder);
+			var segundo = new Pokemon(grupoDos, + color, + poderDeAtaque);
+			primer.atacar(segundo);
+		}
+
+		res.innerHTML ="<br>" + primer.nombre + " ataca a " + segundo.nombre + " y " + segundo.nombre +" tiene una vida de : " + segundo.vida;
+}
